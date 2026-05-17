7e113e45857f0383d381974550d9e2118cbd0c69
alba8558 <albagh85@gmail.com>
2026-05-17 10:16:26 +0200

    Pàs 2: Clàusula de guarda pel Martell de Thor

--- src/main/java/com/tuempresa/Magatzem.java
+++ src/main/java/com/tuempresa/Magatzem.java
@@ -14,11 +14,13 @@ class Magatzem {
     }
 
     private void actualitzarArticleIndividual(Article article) {
+        if (article.nom.equals("Martell de Thor (Llegendari)")) {
+            return;
+        }
+
         if (!article.nom.equals("Formatge Gidurat") && !article.nom.equals("Entrades per al Concert del Trobador")) {
             if (article.qualitat > 0) {
-                if (!article.nom.equals("Martell de Thor (Llegendari)")) {
-                    article.qualitat = article.qualitat - 1;
-                }
+                article.qualitat = article.qualitat - 1;
             }
         } else {
             if (article.qualitat < 50) {
@@ -39,17 +41,13 @@ class Magatzem {
             }
         }
 
-        if (!article.nom.equals("Martell de Thor (Llegendari)")) {
-            article.diesPerVendre = article.diesPerVendre - 1;
-        }
+        article.diesPerVendre = article.diesPerVendre - 1;
 
         if (article.diesPerVendre < 0) {
             if (!article.nom.equals("Formatge Gidurat")) {
                 if (!article.nom.equals("Entrades per al Concert del Trobador")) {
                     if (article.qualitat > 0) {
-                        if (!article.nom.equals("Martell de Thor (Llegendari)")) {
-                            article.qualitat = article.qualitat - 1;
-                        }
+                        article.qualitat = article.qualitat - 1;
                     }
                 } else {
                     article.qualitat = 0;
