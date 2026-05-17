e0c98fb358449726d08296be96de11dbd6d2e6a8
alba8558 <albagh85@gmail.com>
2026-05-17 10:10:52 +0200

    Pàs 1: Estructura base i tests verds

---
+++ src/main/java/com/tuempresa/Magatzem.java
@@ -0,0 +1,64 @@
+package com.tuempresa;
+
+class Magatzem {
+    Article[] articles;
+
+    public Magatzem(Article[] articles) {
+        this.articles = articles;
+    }
+
+    public void actualitzarEstat() {
+        for (int i = 0; i < articles.length; i++) {
+            actualitzarArticleIndividual(articles[i]);
+        }
+    }
+
+    private void actualitzarArticleIndividual(Article article) {
+        if (!article.nom.equals("Formatge Gidurat") && !article.nom.equals("Entrades per al Concert del Trobador")) {
+            if (article.qualitat > 0) {
+                if (!article.nom.equals("Martell de Thor (Llegendari)")) {
+                    article.qualitat = article.qualitat - 1;
+                }
+            }
+        } else {
+            if (article.qualitat < 50) {
+                article.qualitat = article.qualitat + 1;
+
+                if (article.nom.equals("Entrades per al Concert del Trobador")) {
+                    if (article.diesPerVendre < 11) {
+                        if (article.qualitat < 50) {
+                            article.qualitat = article.qualitat + 1;
+                        }
+                    }
+                    if (article.diesPerVendre < 6) {
+                        if (article.qualitat < 50) {
+                            article.qualitat = article.qualitat + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!article.nom.equals("Martell de Thor (Llegendari)")) {
+            article.diesPerVendre = article.diesPerVendre - 1;
+        }
+
+        if (article.diesPerVendre < 0) {
+            if (!article.nom.equals("Formatge Gidurat")) {
+                if (!article.nom.equals("Entrades per al Concert del Trobador")) {
+                    if (article.qualitat > 0) {
+                        if (!article.nom.equals("Martell de Thor (Llegendari)")) {
+                            article.qualitat = article.qualitat - 1;
+                        }
+                    }
+                } else {
+                    article.qualitat = 0;
+                }
+            } else {
+                if (article.qualitat < 50) {
+                    article.qualitat = article.qualitat + 1;
+                }
+            }
+        }
+    }
+}
