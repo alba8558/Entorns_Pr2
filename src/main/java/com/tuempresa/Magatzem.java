7033d5b18f327152464589fe5de935dfedb84016
alba8558 <albagh85@gmail.com>
2026-05-17 10:19:51 +0200

    Pàs 4: Clàusula de guarda per les Entrades i codi completament refactoritzat

--- src/main/java/com/tuempresa/Magatzem.java
+++ src/main/java/com/tuempresa/Magatzem.java
@@ -29,39 +29,30 @@ class Magatzem {
             return;
         }
 
-        if (!article.nom.equals("Entrades per al Concert del Trobador")) {
-            if (article.qualitat > 0) {
-                article.qualitat = article.qualitat - 1;
+        if (article.nom.equals("Entrades per al Concert del Trobador")) {
+            article.diesPerVendre = article.diesPerVendre - 1;
+            if (article.diesPerVendre < 0) {
+                article.qualitat = 0;
+                return;
             }
-        } else {
             if (article.qualitat < 50) {
                 article.qualitat = article.qualitat + 1;
-
-                if (article.nom.equals("Entrades per al Concert del Trobador")) {
-                    if (article.diesPerVendre < 11) {
-                        if (article.qualitat < 50) {
-                            article.qualitat = article.qualitat + 1;
-                        }
-                    }
-                    if (article.diesPerVendre < 6) {
-                        if (article.qualitat < 50) {
-                            article.qualitat = article.qualitat + 1;
-                        }
-                    }
+                if (article.diesPerVendre < 10 && article.qualitat < 50) {
+                    article.qualitat = article.qualitat + 1;
+                }
+                if (article.diesPerVendre < 5 && article.qualitat < 50) {
+                    article.qualitat = article.qualitat + 1;
                 }
             }
+            return;
         }
 
         article.diesPerVendre = article.diesPerVendre - 1;
-
-        if (article.diesPerVendre < 0) {
-            if (!article.nom.equals("Entrades per al Concert del Trobador")) {
-                if (article.qualitat > 0) {
-                    article.qualitat = article.qualitat - 1;
-                }
-            } else {
-                article.qualitat = 0;
-            }
+        if (article.qualitat > 0) {
+            article.qualitat = article.qualitat - 1;
+        }
+        if (article.diesPerVendre < 0 && article.qualitat > 0) {
+            article.qualitat = article.qualitat - 1;
         }
     }
 }
